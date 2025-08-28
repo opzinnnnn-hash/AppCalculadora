ae191b90180de15bd52b225931964d0180e59a7e
opzinnnnn-hash <opzinnnnn@gmail.com>
2025-08-28 15:28:25 -0300

    Commit 2 - Add Divisão e Multi
    Adicionando os métodos Multiplicação e Divisão

--- src/Calculadora.java
+++ src/Calculadora.java
@@ -1,9 +1,9 @@
 public class Calculadora {
-    public static int soma (int a, int b) {
-        return a + b;
+    public static int multiplicacao (int a, int b) {
+        return a * b;
     }
-    public static int subtracao (int a, int b){
-        return a - b;
+    public static int divisao (int a, int b){
+        return a / b;
     }
 
 }
